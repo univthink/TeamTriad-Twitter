0d54489c4cc0c952488d973e213c3fcb9f0ef418
Matt Fair <matt.j.fair@gmail.com>
2015-11-12 16:36:14 -0500

    added bad bio page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
+  def index
+
+  end
+
   def new
     @user = User.new
+  end
+
+  def show
+    @user = User.find(params['id'])
   end
 
   def create
