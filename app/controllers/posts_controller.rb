d3e730b750b67b04f535243044f1e4edda74c11a
Ben Barnett <benjamin@universallythinking.com>
2015-11-12 16:22:04 -0500

    Ben Barnett New Post Page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,15 @@
 class PostsController < ApplicationController
-  def new
-    @posts = Post.all
-    if current_user_session
-    @post = Post.new
-    render partial: "post/new", layout: "application"
-  else
-    render partial: "post/error", layout: "error"
+  def index
+    render template: "posts/index.html.erb"
   end
 
+
+  def new
+    @posts = Post.all
+    #if current_user_session
+    @post = Post.new
+    render template: "posts/new"
+
+
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get 'posts/new' => 'posts#new'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
