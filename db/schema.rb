cb84fbdc664f6dbb5253586346ebb4ddf58e11ee
Matt Fair <matt.j.fair@gmail.com>
2015-11-12 22:01:40 -0500

    completed bio page and registration form page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112192329) do
+ActiveRecord::Schema.define(version: 20151113022601) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "blurb"
@@ -27,8 +27,9 @@ ActiveRecord::Schema.define(version: 20151112192329) do
     t.string   "email"
     t.string   "password"
     t.integer  "posts_ID"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "profile_picture"
   end
 
 end
