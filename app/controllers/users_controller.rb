d0082cc03a3b0823548f11d3f154a0b19fdd1712
Matt Fair <matt.j.fair@gmail.com>
2015-11-12 22:25:49 -0500

    updated controller to collect all the inputs for creating new user and updating old user.  Also added name field that was left off new user view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,11 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params['id'])
+    render "new"
+  end
+
   def show
     @user = User.find(params['id'])
   end
@@ -21,9 +26,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user =User.find(params['id'])  
+    if @user.update(users_params)
+      flash[:success] = "Account Updated!"
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
+  end
+
   private
 
   def users_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation,:profile_picture,:username,:name)
   end
 end
